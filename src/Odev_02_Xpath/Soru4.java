ec24e9015a6b66bba6eb902d0b3b30881cabc2bc
burak <burak_demi1@web.de>
2024-02-25 18:14:18 +0100

    Soru 4 basariyla tamamlandi

--- src/Odev_02_Xpath/Soru4.java
+++ src/Odev_02_Xpath/Soru4.java
@@ -3,6 +3,8 @@ package Odev_02_Xpath;
 import Utlity.BaseDriver;
 import Utlity.MyFunc;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class Soru4 extends BaseDriver {
     @Test
@@ -20,8 +22,26 @@ public class Soru4 extends BaseDriver {
         driver.get("https://testpages.herokuapp.com/styled/index.html");
 
         MyFunc.Bekle(2);
+        WebElement calculater= driver.findElement(By.xpath("(//div[@class='thumbdesclist']//a[@id='calculatetest'])[1]"));
+        calculater.click();
 
+        MyFunc.Bekle(2);
+        WebElement input1= driver.findElement(By.xpath("//div[@class='centered']//input[@id='number1']"));
+        input1.sendKeys("20");
 
+        MyFunc.Bekle(2);
+        WebElement input2=driver.findElement(By.xpath("//div[@class='centered']//input[@id='number2']"));
+        input2.sendKeys("25");
 
+        MyFunc.Bekle(2);
+        WebElement calculatorButton= driver.findElement(By.xpath("//div[@class='centered']//input[@id='calculate']"));
+        calculatorButton.click();
+
+        MyFunc.Bekle(2);
+        WebElement total= driver.findElement(By.xpath("//div[@class='centered']//span[@id='answer']"));
+
+        System.out.println("Total is = " + total.getText());
+
+        BekleKapat();
     }
 }
