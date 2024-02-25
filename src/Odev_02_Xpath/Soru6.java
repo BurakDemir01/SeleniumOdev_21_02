0d477826eb5189cda72e2d310f238a077fb94300
burak <burak_demi1@web.de>
2024-02-25 21:48:59 +0100

    XPath Soru 6 tamamlandi

--- src/Odev_02_Xpath/Soru6.java
+++ src/Odev_02_Xpath/Soru6.java
@@ -3,6 +3,8 @@ package Odev_02_Xpath;
 import Utlity.BaseDriver;
 import Utlity.MyFunc;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class Soru6 extends BaseDriver {
     @Test
@@ -18,6 +20,18 @@ public class Soru6 extends BaseDriver {
         driver.get("https://testpages.herokuapp.com/styled/index.html");
 
         MyFunc.Bekle(2);
+        WebElement fakeAlerts= driver.findElement(By.xpath("//div[@class='page-body']//a[@id='fakealerttest']"));
+        fakeAlerts.click();
 
+        MyFunc.Bekle(2);
+        WebElement showModel= driver.findElement(By.xpath("(//div[@class='page-body']//input[@class='styled-click-button'])[2]"));
+        showModel.click();
+
+        MyFunc.Bekle(2);
+        WebElement okButton= driver.findElement(By.xpath("//div[@class='dialog-actions']//button"));
+        okButton.click();
+        MyFunc.Bekle(2);
+
+        BekleKapat();
     }
 }
